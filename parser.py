75923d9393033081cb6a2f66473a8e6f1e95ffe7
Nikita Nigmatullin <nnigmat@gmail.com>
2019-02-17 22:40:44 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -5,9 +5,9 @@ def parse(path):
             temp = line.split(' ')
             temp[3] = temp[3].strip('\n')
             text = 'aqua'
-            if temp[2] not in ['img', 'audio', 'video'] and temp[3] != 'aqua.png':
+            if temp[2] not in ['img', 'audio', 'video']:
                 text = open('src//{}//{}'.format(temp[2], temp[3])).read()
-            elif temp[3] != 'aqua.png' :
+            else:
                 text = open(f'src/{temp[2]}/{temp[3]}', 'rb').read()
             res.append({'current':temp[0], 'next':temp[1], 'type':temp[2], 'text':text})
     return res
